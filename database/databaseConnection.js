13bc2876eeebf3203e2094628be1ce58d2349afd
Arpana Sharma <itsmearpana06@gmail.com>
2024-07-11 22:41:57 +0545

    first commit

--- database/databaseConnection.js
+++ database/databaseConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 
 async function connectToDb(){
-   await mongoose.connect("mongodb+srv://basnetmanish089:acesdharan@cluster0.qrpac8w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+   await mongoose.connect("mongodb+srv://Arpana:zxcvbnm1@cluster0.gaahd6g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
    console.log("Database connected")
 }
 
